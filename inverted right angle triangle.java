6cf08ca9e63077e7a2c192b5cded0bd67b7565ca
Jaya-Priyam-K <jayapriyamkumar@gmail.com>
2025-05-31 14:18:50 +0530

    Create inverted right angle triangle.java

---
+++ inverted right angle triangle.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int count = 1;
+        for(int i=4; i>0; i--)
+        {
+            for(int j=i; j>0; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
